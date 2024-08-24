70d1a87a7cea4bd1c75645cfbf06c791a937c938
beerus20 <brucejuniorallain@gmail.com>
2024-08-24 11:12:14 +0300

    en cours

--- dependencies/exec/functions/analyse_arg_utils.c
+++ dependencies/exec/functions/analyse_arg_utils.c
@@ -18,6 +18,7 @@ int	ft_new_arg_len(char *arg, t_env_var *venv)
 	char		*tmp;
 	t_env_var	*t_v;
 
+	end = 0;
 	len = 0;
 	r_len = 0;
 	if (*arg == '"' && arg++)
@@ -25,20 +26,21 @@ int	ft_new_arg_len(char *arg, t_env_var *venv)
 	while (*arg && *arg != end)
 	{
 		tmp = ft_strchr(arg, '$');
-		if (!tmp)
-			break ;
-		r_len += tmp - arg;
-		arg = ++tmp;
-		len = ft_getlen_venv(arg);
-		tmp = ft_substr(arg, 0, len);
-		arg += len;
-		t_v = ft_get_env_var(venv, tmp);
-		r_len += ft_lst_content_len(t_v->content) + (ft_lstsize(t_v->content) - 1);
+		if (!tmp && arg++)
+			r_len++;
+		else
+		{
+			r_len += tmp - arg;
+			arg = ++tmp;
+			len = ft_getlen_venv(arg);
+			arg += len;
+			tmp = ft_substr(arg, 0, len);
+			t_v = ft_get_env_var(venv, tmp);
+			r_len += ft_lst_content_len(t_v->content) + (ft_lstsize(t_v->content) - 1);
+		}
 	}
 	return (r_len);
 }
-
-
 
 int	_skip_venv(char *arg)
 {
