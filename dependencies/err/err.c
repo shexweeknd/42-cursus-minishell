3c35b378639d651ab227b38ef1c2af8703187de3
Herinambinintsoa Nantenaina Ramarosandratana <hramaros@c1r10s2.42antananarivo.mg>
2024-08-28 09:10:44 +0300

    effectue:     - mise en place du template pour le module err todo:     - etude des cas d'erreur et mettre dans un enum     - implementer la fonction verify_prompt

--- dependencies/err/err.c
+++ dependencies/err/err.c
@@ -1,33 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   err.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hramaros <hramaros@student.42Antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/10 03:11:46 by ballain           #+#    #+#             */
-/*   Updated: 2024/08/28 09:02:25 by hramaros         ###   ########.fr       */
+/*   Created: 2024/08/28 08:57:14 by hramaros          #+#    #+#             */
+/*   Updated: 2024/08/28 09:06:35 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "err.h"
 
-# include "cmd.h"
-# include "env.h"
-# include "err.h"
-# include "ft_exec.h"
-# include "history.h"
-# include "struct.h"
-# include <fcntl.h>
-# include <readline/history.h>
-# include <readline/readline.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <sys/types.h>
-# include <sys/wait.h>
-# include <unistd.h>
-
-# define T_S struct sigaction
-
-#endif
+int	verify_prompt(char *prompt)
+{
+	(void)prompt;
+	printf("will serve to verify char * error associated\n");
+	return (1);
+}
--- dependencies/err/includes/err.h
+++ dependencies/err/includes/err.h
@@ -1,33 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   err.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hramaros <hramaros@student.42Antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/10 03:11:46 by ballain           #+#    #+#             */
-/*   Updated: 2024/08/28 09:02:25 by hramaros         ###   ########.fr       */
+/*   Created: 2024/08/28 08:58:01 by hramaros          #+#    #+#             */
+/*   Updated: 2024/08/28 09:06:48 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef ERR_H
+# define ERR_H
 
-# include "cmd.h"
-# include "env.h"
-# include "err.h"
-# include "ft_exec.h"
-# include "history.h"
-# include "struct.h"
-# include <fcntl.h>
+# include "libft.h"
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <stdio.h>
-# include <stdlib.h>
-# include <sys/types.h>
-# include <sys/wait.h>
-# include <unistd.h>
 
-# define T_S struct sigaction
+int verify_prompt(char *prompt);
 
 #endif
