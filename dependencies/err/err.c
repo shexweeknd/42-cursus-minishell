1362e23db27966cc0571ba9511bb344d4e5f5e32
Herinambinintsoa Nantenaina Ramarosandratana <hramaros@c1r10s2.42antananarivo.mg>
2024-09-04 11:02:53 +0300

    effectue:     - gesiton des erreurs de type unexpected token todo:     - forensics des autres erreurs

--- dependencies/err/err.c
+++ dependencies/err/err.c
@@ -6,50 +6,39 @@
 /*   By: hramaros <hramaros@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 08:57:14 by hramaros          #+#    #+#             */
-/*   Updated: 2024/08/30 10:28:55 by hramaros         ###   ########.fr       */
+/*   Updated: 2024/09/04 11:00:03 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../cmd/includes/cmd.h"
+#include "../cmd/includes/cmd_struct.h"
 #include "m_err.h"
 #include "m_err_struct.h"
 
-void	display_error(int err_type, char *message)
+void	display_error(int err_type, t_cmd *cmd)
 {
 	if (err_type == UNEXPECTED_TOKEN)
 	{
-		printf("%s\n", message);
+		printf("Minishell: syntax error near unexpected token `%s'\n",
+			get_link_char(cmd->link_type));
 		return ;
 	}
 }
 
-/**
- * TODO WILL VERIFY ALL SYNTAX RELATED ERRORS
- */
-int	verify_token(char *prompt)
+int	is_unexpected_token(t_cmd *cmd)
 {
-	while (ft_isspace(*prompt))
-		prompt++;
-	if (*prompt == '|')
-		return (display_error(UNEXPECTED_TOKEN,
-				"Minishell: syntax error near unexpected token `|'"), 0);
-	
-	return (1);
+	while (cmd)
+	{
+		if (!cmd->nb_arg)
+			return (display_error(UNEXPECTED_TOKEN, cmd), 1);
+		cmd = cmd->next;
+	}
+	return (0);
 }
 
-/**
- * TODO WILL VERIFY ALL EXECUTION RELATED ERRORS
- */
-int	verify_cmd(char *prompt)
+t_cmd	*verify_cmds(t_cmd *cmd)
 {
-	(void)prompt;
-	return (1);
-}
-
-int	verify_prompt(char *prompt)
-{
-	if (!verify_token(prompt))
-		return (0);
-	else if (!verify_cmd(prompt))
-		return (0);
-	return (1);
+	if (is_unexpected_token(cmd))
+		return (ft_free_cmds(cmd), NULL);
+	return (cmd);
 }
--- dependencies/err/functions/display_err_utils.c
+++ dependencies/err/functions/display_err_utils.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   m_err.h                                            :+:      :+:    :+:   */
+/*   display_err_utils.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hramaros <hramaros@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/28 08:58:01 by hramaros          #+#    #+#             */
-/*   Updated: 2024/09/04 10:59:57 by hramaros         ###   ########.fr       */
+/*   Created: 2024/09/04 10:46:07 by hramaros          #+#    #+#             */
+/*   Updated: 2024/09/04 10:59:48 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef M_ERR_H
-# define M_ERR_H
+#include "../includes/m_err.h"
 
-# include "../../cmd/includes/cmd_struct.h"
-# include "libft.h"
-# include <readline/history.h>
-# include <readline/readline.h>
-# include <stdio.h>
-
-t_cmd	*verify_cmds(t_cmd *cmd);
-
-// UTILS FUNCTIONS FOR ERROR HANDLERS
-char	*get_link_char(t_link link);
-
-#endif
+char	*get_link_char(t_link link)
+{
+	if (link == PIPE)
+		return ("|");
+	else if (link == BG)
+		return ("&");
+	else if (link == OR)
+		return ("||");
+	else if (link == AND)
+		return ("&&");
+	else
+		return ("");
+}
