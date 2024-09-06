df4a9f5d9945abc3a23e293d37be6fde91b52c01
Herinambinintsoa Nantenaina Ramarosandratana <hramaros@c1r10s2.42antananarivo.mg>
2024-09-06 11:53:29 +0300

    effectue:     - finalisation de la gestion d'erreur unexpected token pour: >, <, >>, <<, <<<, >|, <>, |, ||, &, &&, |& todo:     - tester la gestion d'erreur pre-parsing     - implementer le main commun dans le repository

--- dependencies/err/functions/display_err_utils.c
+++ dependencies/err/functions/display_err_utils.c
@@ -6,14 +6,14 @@
 /*   By: hramaros <hramaros@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 10:46:07 by hramaros          #+#    #+#             */
-/*   Updated: 2024/09/06 09:16:01 by hramaros         ###   ########.fr       */
+/*   Updated: 2024/09/06 11:48:13 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../cmd/includes/cmd.h"
 #include "../includes/m_err.h"
 
-void	display_unexpected_token(char *token)
+void	display_unexpected_chev(char *token)
 {
 	if (!ft_strncmp(token, ">>", 2))
 		printf("Minishell: syntax error near unexpected token `%.2s'\n", token);
@@ -31,4 +31,31 @@ void	display_unexpected_token(char *token)
 		printf("Minishell: syntax error near unexpected token `%.1s'\n", token);
 	else if (!ft_strncmp(token, "newline", 7))
 		printf("Minishell: syntax error near unexpected token `%.7s'\n", token);
+}
+
+void	display_unexpected_pipe(char *token)
+{
+	if (!ft_strncmp(token, "||", 2))
+		printf("Minishell: syntax error near unexpected token `%.2s'\n", token);
+	else if (!ft_strncmp(token, "|&", 2))
+		printf("Minishell: syntax error near unexpected token `%.2s'\n", token);
+	else if (!ft_strncmp(token, "|", 1))
+		printf("Minishell: syntax error near unexpected token `%.1s'\n", token);
+	return ;
+}
+
+void	display_unexpected_and(char *token)
+{
+	if (!ft_strncmp(token, "&&", 2))
+		printf("Minishell: syntax error near unexpected token `%.2s'\n", token);
+	else if (!ft_strncmp(token, "&", 1))
+		printf("Minishell: syntax error near unexpected token `%.1s'\n", token);
+	return ;
+}
+
+void	display_unexpected_token(char *token)
+{
+	display_unexpected_chev(token);
+	display_unexpected_pipe(token);
+	display_unexpected_and(token);
 }
--- dependencies/err/functions/unexpected_err.c
+++ dependencies/err/functions/unexpected_err.c
@@ -6,18 +6,18 @@
 /*   By: hramaros <hramaros@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 14:29:29 by hramaros          #+#    #+#             */
-/*   Updated: 2024/09/06 09:27:00 by hramaros         ###   ########.fr       */
+/*   Updated: 2024/09/06 11:51:27 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../cmd/includes/cmd.h"
 #include "../includes/m_err.h"
 
-int	appcheck(char *prompt, int *flag)
+int	chev_check(char *prompt, int *flag, int chev_type)
 {
 	int	cursor;
 
-	cursor = 2;
+	cursor = chev_type;
 	cursor += _skip_space(&prompt[cursor]);
 	if (ft_is_delimiter(prompt[cursor]) || ft_is_redirect(prompt[cursor]))
 		return (*flag = 1, display_unexpected_token(&prompt[cursor]), 0);
@@ -29,22 +29,38 @@ int	appcheck(char *prompt, int *flag)
 	return (cursor);
 }
 
-int	routcheck(char *prompt, int *flag)
+int	pipe_check(char *prompt, int *flag, int chev_type)
 {
 	int	cursor;
 
-	cursor = 1;
-	if (prompt[cursor] == '>')
-		return (0);
+	cursor = chev_type;
 	cursor += _skip_space(&prompt[cursor]);
-	if (ft_is_delimiter(prompt[cursor]) || ft_is_redirect(prompt[cursor]))
-		return (*flag = 1, display_unexpected_token(&prompt[cursor]), 0);
 	if (!prompt[cursor])
+		return (display_unexpected_token("|"), 1);
+	if ((prompt[cursor] == '<' || prompt[cursor] == '>')
+		&& !(ft_is_delimiter(prompt[cursor + 1]) || ft_is_redirect(prompt[cursor
+					+ 1])))
 	{
 		display_unexpected_token("newline");
 		*flag = 1;
 	}
+	else if (ft_is_delimiter(prompt[cursor]) || ft_is_redirect(prompt[cursor]))
+		return (*flag = 1, display_unexpected_token(&prompt[cursor]), 0);
 	return (cursor);
+}
+
+int	check_op_at_start(char *prompt)
+{
+	int	cursor;
+
+	cursor = 0;
+	cursor += _skip_space(prompt);
+	if (!ft_strncmp(&prompt[cursor], "||", 2) || !ft_strncmp(&prompt[cursor],
+			"&&", 2))
+		return (display_unexpected_token(&prompt[cursor]), 0);
+	else if (prompt[cursor] == '|' || prompt[cursor] == '&')
+		return (display_unexpected_token(&prompt[cursor]), 0);
+	return (1);
 }
 
 int	is_unexpected(char *prompt)
@@ -52,18 +68,21 @@ int	is_unexpected(char *prompt)
 	int	flag;
 
 	flag = 0;
-	while (*prompt)
+	if (!check_op_at_start(prompt))
+		return (1);
+	while (*prompt && !flag)
 	{
-		if (!ft_strncmp(prompt, ">>", 2) && !flag)
-			prompt += appcheck(prompt, &flag);
-		else if (*prompt == '>' && !flag)
-			prompt += routcheck(prompt, &flag);
+		if ((!ft_strncmp(prompt, ">>", 2) || !ft_strncmp(prompt, "<<", 2))
+			&& !flag)
+			prompt += chev_check(prompt, &flag, 2);
+		else if ((*prompt == '>' || *prompt == '<') && !flag)
+			prompt += chev_check(prompt, &flag, 1);
+		else if (!ft_strncmp(prompt, "||", 2) && !flag)
+			prompt += pipe_check(prompt, &flag, 2);
+		else if (*prompt == '|' && !flag)
+			prompt += pipe_check(prompt, &flag, 1);
 		else
 			prompt++;
 	}
-	// if (*cmd == '<')
-	// 	return (handle_rin(cmd + 1));
-	// if (*cmd == '&')
-	// 	return (handle_and(cmd + 1));
 	return (flag);
 }
