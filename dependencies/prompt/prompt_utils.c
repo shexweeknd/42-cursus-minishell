ca0a1e0f5c0425c556d7a8276efe4f508d02242e
Herinambinintsoa Nantenaina Ramarosandratana <hramaros@c1r10s2.42antananarivo.mg>
2024-09-09 09:07:06 +0300

    effectue:     - ajout de '&&' en tant que uncomplete_prompt si il est a la fin todo:     - forensics more

--- dependencies/prompt/prompt_utils.c
+++ dependencies/prompt/prompt_utils.c
@@ -6,7 +6,7 @@
 /*   By: hramaros <hramaros@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 11:33:07 by hramaros          #+#    #+#             */
-/*   Updated: 2024/09/07 12:55:09 by hramaros         ###   ########.fr       */
+/*   Updated: 2024/09/09 09:05:21 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 char	*ft_join_prompt(char *prompt, t_env *env)
 {
 	char	*result;
+	char	*tmp;
 	char	*new_prompt;
 	char	*col_ps_two;
 
@@ -29,8 +30,10 @@ char	*ft_join_prompt(char *prompt, t_env *env)
 		free(new_prompt);
 		new_prompt = readline(col_ps_two);
 	}
-	result = ft_strjoin(prompt, new_prompt);
-	return (free(col_ps_two), free(prompt), free(new_prompt), result);
+	tmp = ft_strjoin(" ", new_prompt);
+	result = ft_strjoin(prompt, tmp);
+	return (free(col_ps_two), free(prompt), free(tmp), free(new_prompt),
+		result);
 }
 
 int	is_pipe_at_end(char *prompt)
@@ -47,10 +50,29 @@ int	is_pipe_at_end(char *prompt)
 	return (0);
 }
 
+int	is_and_at_end(char *prompt)
+{
+	int	cursor;
+
+	cursor = 0;
+	while (prompt[cursor])
+		cursor++;
+	while (cursor && prompt[cursor--] == ' ')
+		;
+	if (prompt[cursor] == '&' && cursor >= 2)
+	{
+		if (prompt[cursor - 1] == '&')
+			return (1);
+	}
+	return (0);
+}
+
 // TODO FORENSICS OF PROMPT THAT DISPLAYS PS2
 int	is_uncomplete_prompt(char *prompt)
 {
 	if (is_pipe_at_end(prompt))
+		return (1);
+	if (is_and_at_end(prompt))
 		return (1);
 	return (0);
 }
